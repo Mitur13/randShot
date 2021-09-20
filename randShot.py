fb030f4a9c07ce5df4ee6dcd8b6801c54e242ca0
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-20 16:38:06 +0100

    Added empty window comment window

--- randShot.py
+++ randShot.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 import webbrowser
 import random
 import string
@@ -19,16 +20,28 @@ def openlink():
 	two_letters = get_random_string(2)		#get string of 2 letters
 	numbers = str(format(random.randint(0,9999), '04d'))	#generate string of digits 0000-9999
 	mylink = ["http://prnt.sc/", two_letters, numbers]	
+	global full_link			#makes it possible to pull full_link outside this function
 	full_link = "".join(mylink)		#combining the list to create a link
 	webbrowser.open(full_link)		#open a link in the browser
 	print(full_link)		#print the link in the command line (history)
 
-### Add functionality: save the link to a .txt file (append)
-	### Popup with text input window - 'Add note to the link' - save in file along the link
+
+def savelink():			#saves the current link to .txt file
+	fin = open("Saved_Links.txt", "a")
+
+	savelink = Tk()	#open new window asking for user input
+
+	# savelink.title("Add comment")
+	#comment = Entry
+
+	tkinter.messagebox.showinfo("Link saved", "Link succefully saved to Saved_Links.txt")	### Popup with text input window - 'Add note to the link' - save in file along the link
+	fin.write(full_link + comment)	#pass input to 
+	fin.close()
+	savelink.mainloop()
 
 btnopenlink = Button(window,text="Open Random LightShot screenshot!", width=30,height=2,command=openlink).pack()
 
-btnsavelink = Button(window,text="Save current link", width=25,height=1)
+btnsavelink = Button(window,text="Save current link", width=25,height=1,command=savelink)
 btnsavelink.place(relx=0.5, y=60, anchor=CENTER)
 
 window.mainloop()
