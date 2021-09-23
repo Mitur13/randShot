ebd8242537f465fb20894a2e55596e43fd4cc574
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-23 22:40:45 +0100

    Popup window with input field
    Need to pass the input to the variable - either by adding a button that does that and closes the window or (preferably) after the user closes the window.

--- randShot.py
+++ randShot.py
@@ -5,13 +5,14 @@ import random
 import string
 import keyboard
 import time
+import tkinter as tk
 
 window = Tk()
 window.title('Random LightShot')
 window.geometry('300x80')
 
 def get_random_string(size=2, chars=string.ascii_lowercase):	#define function to create random sting
-    return ''.join(random.choice(chars) for i in range(size))	#return the string	
+	return ''.join(random.choice(chars) for i in range(size))	#return the string	
 
 def openlink():
 	keyboard.press_and_release('alt+tab')	#focus back on browser window
@@ -21,23 +22,37 @@ def openlink():
 	numbers = str(format(random.randint(0,9999), '04d'))	#generate string of digits 0000-9999
 	mylink = ["http://prnt.sc/", two_letters, numbers]	
 	global full_link			#makes it possible to pull full_link outside this function
-	full_link = "".join(mylink)		#combining the list to create a link
-	webbrowser.open(full_link)		#open a link in the browser
+	full_link = ''.join(mylink)		#combining the list to create a link
+	webbrowser.open(full_link)		#open the link in the browser
 	print(full_link)		#print the link in the command line (history)
 
-
+def close_win(popupwin):	#define a function to close the popup window
+   popupwin.destroy()
+def insert_val(e):
+   e.insert(0, "Hello World!")
+    
 def savelink():			#saves the current link to .txt file
-	fin = open("Saved_Links.txt", "a")
+	fin = open("Saved_Links.txt", "a")  #open .txt file 
 
-	savelink = Tk()	#open new window asking for user input
+	popupwin = Tk()	#open new window asking for user input
+	popupwin.geometry("250x60")
+	popupwin.title("Add comment")
 
-	# savelink.title("Add comment")
-	#comment = Entry
 
-	tkinter.messagebox.showinfo("Link saved", "Link succefully saved to Saved_Links.txt")	### Popup with text input window - 'Add note to the link' - save in file along the link
-	fin.write(full_link + comment)	#pass input to 
-	fin.close()
-	savelink.mainloop()
+	### need to use .get method to pass the user input to the variable
+	comment = Entry(popupwin, width=30)) #create an entry widget
+	comment.pack()
+	comment = str(comment)	#turn Entry into string
+
+	###Add button that passes the users entry to the comment variable
+
+	###Make so the comment gets saved after the window gets closed
+
+		###tkinter.messagebox.showinfo("Link saved", "Link succefully saved to Saved_Links.txt")	### Popup with text input window - 'Add note to the link' - save in file along the link
+	popupwin.mainloop()
+	 
+	fin.write(full_link + comment + "\n")	#pass input to the file
+	fin.close()						#close the .txt file
 
 btnopenlink = Button(window,text="Open Random LightShot screenshot!", width=30,height=2,command=openlink).pack()
 
